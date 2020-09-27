f14cae90be62a942ec85981bc6709ef7eafba13a
axchi72 <axchi72@gmail.com>
2020-09-27 13:51:15 -0600

    Implementando el template

---
+++ resources/views/theme/lte/layout.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>AdminLTE 3 | Blank Page</title>
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="{{asset("assets/$theme/plugins/fontawesome-free/css/all.min.css")}}">
+        <!-- Ionicons -->
+        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+        <!-- overlayScrollbars -->
+        <link rel="stylesheet" href="{{asset("assets/$theme/dist/css/adminlte.min.css")}}">
+        <!-- Google Font: Source Sans Pro -->
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    </head>
+    <body class="hold-transition sidebar-mini">
+        <!-- Site wrapper -->
+        <div class="wrapper">
+            <!-- Navbar -->
+            @include("theme/$theme/header")
+            <!-- /.navbar -->
+            <!-- Main Sidebar Container -->
+            @include("theme/$theme/sidebar")
+            <!-- /.main Sidebar Container -->
+            <!-- Content -->
+            <div class="content-wrapper">
+
+            </div>
+            <!-- /.content -->
+            <!-- Footer -->
+            @include("theme/$theme/footer")
+            <!-- /.footer -->
+        </div>
+        <!-- jQuery -->
+        <script src="{{asset("assets/$theme/plugins/jquery/jquery.min.js")}}"></script>
+        <!-- Bootstrap 4 -->
+        <script src="{{asset("assets/$theme/plugins/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
+        <!-- AdminLTE App -->
+        <script src="{{asset("assets/$theme/dist/js/adminlte.min.js")}}"></script>
+        <!-- AdminLTE for demo purposes -->
+        <script src="{{asset("assets/$theme/dist/js/demo.js")}}"></script>
+    </body>
+</html>
